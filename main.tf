b64c64c78b632007b6894fecbf1e584d42570de8
ogi <orgertaabazi@gmail.com>
2024-12-10 07:30:36 +0000

    update

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "devsecopsproject0" {
-  bucket = "devsecopsproject0"
+resource "aws_s3_bucket" "ogiprojects" {
+  bucket = "ogiprojects"
 }
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "devsecopsproject0"
+    bucket         = "ogiprojects"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-west-2"
   }
